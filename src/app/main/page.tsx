e7137e02f1c2c99c4971c0ee491c03f217b97d62
MobasirSarkar <mobasirgithubonly@gmail.com>
2024-08-06 23:27:42 +0530

    redesign

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,11 +1,9 @@
 "use client"
 import ModeToggle from '@/components/providers/mode-toggle'
-import { BackGround } from '@/components/ui/_animatedComp/background'
 import React from 'react'
 
 export default function Landing() {
    return (
-
       <section className='centerbox w-full h-full z-10 bg-verticalLine' id="landing" >
          <ModeToggle />
       </section>
